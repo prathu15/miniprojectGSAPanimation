508114e9311edd03fe4998229df28d7b4023051c
Prathu Dwivedi <prathudwivedi241@gmail.com>
2024-05-06 19:53:29 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+-GSAP
+-What is Animation
+GSAP Introduction
+GSAP animation basics
+gsap.to() gsap.from()
+Some other features
+Gsap Timeline and how to control it
+Gsap ScrollTrigger
+Pinning in ScrollTrigger
+some other pluggins
+Project with GSAP
+ EK example le raha hun hamare sheryians  mein kya hotha hai bhopal  maan lete hai b-28 batch Goha bhopal sab alag main mere yaha par problem par 
+ mera naam prathu hai
---
+++ script.js
@@ -0,0 +1,28 @@
+ var menu = document.querySelector("#nav i")
+ var close = document.querySelector("#full i")
+
+var tl = gsap.timeline()
+
+tl.to('#full',{
+    right:0,
+    duration: 0.8
+})
+tl.from("#full h4",{
+ x:150,
+ duration: 0.2,
+ stagger: 0.24,
+ opacity:0
+})
+tl.from("#full i",{
+    opacity:0
+})
+tl.pause()
+
+menu.addEventListener("click", function(){
+    tl.play()
+})
+close.addEventListener("click",function(){
+    tl.reverse()
+})
+
+
